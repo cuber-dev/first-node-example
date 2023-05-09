a6439f5122b07a724699232422bdc9c4e927815d
anukitty <adepunandhu9@gmail.com>
2023-05-09 19:07:36 +0530

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// write comments
+
+ 
+// npm - global command, comes with node
+const lodash = require('lodash')
+
+
+const items = [1,[2,[3,[4]]]]
+const newItems = lodash.flattenDeep(items)
+console.log(newItems)
+console.log(lodash)
